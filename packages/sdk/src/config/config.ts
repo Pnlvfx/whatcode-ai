e98f7db98c998487772a5c3fe5e39bf35f17ce6c
Simone Gauli <simonegauli@gmail.com>
2026-04-28 14:45:53 +0200

    revert env on sdk

--- packages/sdk/src/config/config.ts
+++ packages/sdk/src/config/config.ts
@@ -1,5 +1,12 @@
 import { homedir } from 'node:os';
 import path from 'node:path';
+import { logger } from '../logger.ts';
+
+try {
+  process.loadEnvFile();
+} catch {
+  logger.debug('env', 'no .env file present, using defaults');
+}
 
 // eslint-disable-next-line no-restricted-properties
 export const SERVER_URL = process.env['WHATCODE_SERVER_URL'] ?? 'https://api.whatcode.app';
