e6c51a04a06047fddb6feb55d4abb85029320d8f
Simone Gauli <simonegauli@gmail.com>
2026-05-12 16:31:21 +0200

    chore(example): fix typo

--- apps/example/src/server.ts
+++ apps/example/src/server.ts
@@ -1,7 +1,7 @@
 import { createWhatcodeServer } from '@whatcode-ai/sdk';
 
 /** This will:
- * 1. Start opencocode if not alreadyd running.
+ * 1. Start opencocode if not already running.
  * 2. Start tailscale, you can use the app from everywhere.
  * 3. Start our daemon, improve the experience on the app and allow you to receive notifications.
  */
