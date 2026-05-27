e4e75ead7e51eed7901e508a9687953b52576f86
Simone Gauli <simonegauli@gmail.com>
2026-05-27 12:11:35 +0200

    fix: secure qr scanning

--- packages/whatcode/src/constants.ts
+++
@@ -1,1 +0,0 @@
-export const CLIENT_URL = 'https://www.whatcode.app';
--- packages/whatcode/src/qrcode.ts
+++ packages/whatcode/src/qrcode.ts
@@ -1,9 +1,9 @@
 import qrcode from 'qrcode-terminal';
-import { CLIENT_URL } from './constants.ts';
+
 
 export const printQrCode = (url: string, password?: string): void => {
   const params = new URLSearchParams({ url });
   if (password) params.set('password', password);
-  const deepLink = `${CLIENT_URL}/connect?${params.toString()}`;
+  const deepLink = `whatcode://connect?${params.toString()}`;
   qrcode.generate(deepLink, { small: true });
 };
