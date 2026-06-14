c4e68102115ff59bccbedbb32b32acb06cb2a5c7
Simone Gauli <simonegauli@gmail.com>
2026-06-14 13:20:46 +0200

    chore: update deps

--- packages/sdk/src/client.ts
+++ packages/sdk/src/client.ts
@@ -1,3 +1,6 @@
+// TODO [2026-08-08] Eysia type portability issue
+// eslint-disable-next-line sonarjs/no-internal-api-use
+import type * as _ from '../../../node_modules/@elysia/eden/dist/types-0YqzVuGd.js';
 import type { App } from '@/server/server';
 import { treaty } from '@elysia/eden';
 import { SERVER_URL } from './config/constants.ts';
